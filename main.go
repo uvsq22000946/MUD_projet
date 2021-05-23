5ca865a0215c77a74f0ce49f9c68fade38f4b9fb
Maxime EBRAN <maxime.ebran@ens.uvsq.fr>
2021-05-23 20:33:11 +0200

    Fin de la video 1

--- main.go
+++ main.go
@@ -3,13 +3,54 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"net"
 )
 
-func handleConnection(conn net.Conn) error {
-	log.Println("I got a connection")
+type MessageEvent struct {
+	msg string
+}
 
+type UserJoinedEvent struct {
+}
+
+type ClientInput struct {
+	user  *User
+	event interface{}
+}
+
+type User struct {
+	name    string
+	session *Session
+}
+
+type Session struct {
+	conn net.Conn
+}
+
+func (s *Session) WriteLine(str string) error {
+	_, err := s.conn.Write([]byte(str + "\r\n"))
+	return err
+}
+
+type World struct {
+	users []*User
+}
+
+func generationName() string {
+	return fmt.Sprintf("User %d", rand.Intn(100)+1)
+}
+
+func handleConnection(conn net.Conn, inputChannel chan ClientInput) error {
 	buf := make([]byte, 4096)
+
+	session := &Session{conn}
+	user := &User{name: generationName(), session: session}
+
+	inputChannel <- ClientInput{
+		user,
+		&UserJoinedEvent{},
+	}
 
 	for {
 		n, err := conn.Read(buf)
@@ -23,21 +64,14 @@ func handleConnection(conn net.Conn) error {
 		msg := string(buf[0 : n-2])
 		log.Println("Received message:", msg)
 
-		resp := fmt.Sprintf("Vous avez dis, \"%s\"\r\n", msg)
-		n, err = conn.Write([]byte(resp))
-		if err != nil {
-			return err
-		}
-		if n == 0 {
-			log.Println("Zero bytes, closing connection")
-			break
-		}
-	}
+		e := ClientInput{user, &MessageEvent{msg}}
+		inputChannel <- e
 
+	}
 	return nil
 }
 
-func startserver() error {
+func startserver(eventChannel chan ClientInput) error {
 	log.Println("Starting server")
 	ln, err := net.Listen("tcp", ":8080")
 	if err != nil {
@@ -50,7 +84,7 @@ func startserver() error {
 			continue
 		}
 		go func() {
-			if err := handleConnection(conn); err != nil {
+			if err := handleConnection(conn, eventChannel); err != nil {
 				log.Println("Error handling connection", err)
 				return
 			}
@@ -58,10 +92,41 @@ func startserver() error {
 	}
 }
 
+func startGameLoop(clientInputChannel <-chan ClientInput) {
+	w := &World{}
+	for input := range clientInputChannel {
+		switch event := input.event.(type) {
+		case *MessageEvent:
+			fmt.Println("Message recu", event.msg)
+			// TODO: error handing
+			input.user.session.WriteLine(fmt.Sprintf("Vous avez dis, \"%s\"\r\n", event.msg))
+			for _, user := range w.users {
+				if user != input.user {
+					user.session.WriteLine(fmt.Sprintf("%s a dis, \"%s\"", input.user.name, event.msg))
+				}
+			}
+		case *UserJoinedEvent:
+			fmt.Println("User joined :", input.user.name)
+			w.users = append(w.users, input.user)
+			input.user.session.WriteLine(fmt.Sprintf("Bienvenue %s", input.user.name))
+			for _, user := range w.users {
+				if user != input.user {
+					user.session.WriteLine(fmt.Sprintf("%s entre dans la salle", input.user.name))
+				}
+			}
+		}
+	}
+}
+
 func main() {
-	err := startserver()
+
+	ch := make(chan ClientInput)
+
+	go startGameLoop(ch)
+
+	err := startserver(ch)
 	if err != nil {
 		log.Fatal(err)
+
 	}
-	println("hello world")
 }
